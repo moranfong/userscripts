9ec3c0d2f7547768ed057f3f7c8c2439008c68d6
moranfong <moranfong@gmail.com>
2025-10-22 12:48:25 +0800

    add text-autospace

---
+++ src/text-autospace.js
@@ -0,0 +1,11 @@
+// Text AutoSpace - 文本自动空格
+(function() {
+    'use strict';
+    
+    const css = String.raw;
+    GM_addStyle(css`
+        :root {
+            text-autospace: normal;
+        }
+    `);
+})();
