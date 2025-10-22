e247f5ada5d6b15ece96fe3d6134cfc11c6ae7f1
moranfong <moranfong@gmail.com>
2025-10-22 13:18:00 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # UserScripts
+
+## Code
+[Text Auto Space](https://github.com/moranfong/userscripts/blob/main/src/text-autospace.js)
+
+## Download
+[Text Auto Space](https://greasyfork.org/zh-CN/scripts/553348-text-auto-space)
--- src/text-autospace.js
+++ src/text-autospace.js
@@ -1,4 +1,19 @@
-// Text AutoSpace - 文本自动间距
+// ==UserScript==
+// @name               文本自动间距
+// @name:EN            Text Auto Space
+// @name:zh-TW         文字自動间距
+// @version            1.0.1
+// @description        这个脚本为现代浏览器添加了 text-autospace CSS 特性
+// @description:EN     This script adds the text-autospace CSS feature for modern browsers
+// @description:zh-TW  這個腳本為現代瀏覽器添加了 text-autospace CSS 特性
+// @author             Moran Fong
+// @contributionURL    https://github.com/moranfong/userscripts/blob/main/src/text-autospace.js
+// @namespace          http://tampermonkey.net/
+// @match              *://*/*
+// @license MIT
+// @grant              GM_addStyle
+// ==/UserScript==
+ 
 (function() {
     'use strict';
     
